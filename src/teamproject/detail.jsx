468db5dfc1c79fa10730083d2a2f81e4c128c42f
LIVINGROOM-DESK\Home <ebyul96428@naver.com>
2023-01-16 20:43:38 +0900

    수정(스크롤 미포함)

--- src/teamproject/detail.jsx
+++ src/teamproject/detail.jsx
@@ -2,18 +2,16 @@ import React, { useRef, useState, useEffect } from "react";
 import './detail.css';
 
 function Main(){
-    // 클릭시 스크롤 이동
-    // const idRef1 = useRef('content1');
-    // const idRef2 = useRef('content2');
-    // const idRef3 = useRef('content3');
-    // const idRef4 = useRef('content4');
+    // 클릭시 스크롤 이동[ 작동안됨, 임시로 a태그로 해결]    
+    const idRef1 = useRef(null);
+    const idRef2 = useRef(null);
+    const idRef3 = useRef(null);
 
-    // const onScrollClick = (id = String) => {
-    //     if(id === 'content1') idRef1.current?.scrollIntoView({ behavior: 'smooth' });
-    //     if(id === 'content2') idRef2.current?.scrollIntoView({ behavior: 'smooth' });
-    //     if(id === 'content3') idRef3.current?.scrollIntoView({ behavior: 'smooth' });
-    //     if(id === 'content4') idRef4.current?.scrollIntoView({ behavior: 'smooth' });
-    // };
+    const onScrollClick = (id) => {
+        if(id === 'content1') idRef1.current?.scrollIntoView({ behavior: 'smooth' });
+        if(id === 'content2') idRef2.current?.scrollIntoView({ behavior: 'smooth' });
+        if(id === 'content3') idRef3.current?.scrollIntoView({ behavior: 'smooth' });
+    };
     // 스크롤시 상단 고정
     const [ScrollY, setScrollY] = useState(0);
     const [ScrollActive, setScrollActive] = useState(false);
@@ -52,24 +50,50 @@ function Main(){
             </div> 
             <div className= {ScrollActive ? 'detail_menu fixed' : 'detail_menu'}>
                 <div className='menu_list'>
-                    <div className='menu'><a href="#content1">강의소개</a></div>
-                    <div className='menu'><a href="#content2">강사소개</a></div>
-                    <div className='menu'><a href="#content3">커리큘럼</a></div>
-                    <div className='menu'><a href="#content4">수강평</a></div>
+                    <div className='menu'><button type="button" onClick={() => onScrollClick()}>강의소개</button></div>
+                    <div className='menu'><button type="button" onClick={() => onScrollClick()}>커리큘럼</button></div>
+                    <div className='menu'><button type="button" onClick={() => onScrollClick()}>수강평</button></div>
                 </div>
             </div> 
             <div className='detail_content'>
                 <div className='detail_content_main'>
-                    <div id='content1' className='con'>
+                    <div id='content1' className= {ScrollActive ? 'con scroll' : 'con'} ref={idRef1}>
                         <h1>강의소개</h1>
+                        <p>강의 설명<br/>
+                            - 이강의는 ~~~~~~~~~~~하는 강의입니다.<br/>
+                        </p>
+                        <p>도움되실 대상(필요없으면 제거)<br/>
+                            1. aa<br/>
+                            2. bb<br/>
+                            3. cc
+                        </p>
+                        <p style={{fontSize: '32px', fontWeight: 'bold'}}>강의 특징</p>
+                        <p style={{lineHeight: '34px'}}>
+                            사용언어 : xxx<br/>
+                            학습방법<br/>
+                            * 1. AAA<br/>
+                            * 2. BBB
+                        </p>
+                        <p>연관강의 링크</p>
                     </div>
-                    <div id='content2' className='con'>
-                        <h1>강사소개</h1>
-                    </div>
-                    <div id='content3' className='con'>
+                    <div id='content2' className='con' ref={idRef2}>
                         <h1>커리큘럼</h1>
+                        <div className="car_list">
+                            <div className="car_title"><p style={{margin: 0, padding: '5px 0' , borderBottom: '1px solid rgb(250, 86, 86)'}}>강의 1</p>
+                                <div className="car_item"><a>강의 1-1. AAA</a></div>
+                                <div className="car_item"><a>강의 1-2. BBB</a></div>    
+                            </div>
+                            <div className="car_title"><p style={{margin: 0, padding: '5px 0' , borderBottom: '1px solid rgb(250, 86, 86)'}}>강의 2</p>
+                                <div className="car_item"><a>강의 2-1. AAA</a></div>
+                                <div className="car_item"><a>강의 2-2. BBB</a></div>    
+                            </div>
+                            <div className="car_title"><p style={{margin: 0, padding: '5px 0' , borderBottom: '1px solid rgb(250, 86, 86)'}}>강의 3</p>
+                                <div className="car_item"><a>강의 3-1. AAA</a></div>
+                                <div className="car_item"><a>강의 3-2. BBB</a></div>    
+                            </div>
+                        </div>
                     </div>
-                    <div id='content4' className='con'>
+                    <div id='content3' className='con'  ref={idRef3}>
                         <h1>수강평</h1>
                         <div className='review_list'>
                             <div className='review_body'>
@@ -78,18 +102,27 @@ function Main(){
                                 <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
                                 <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
                             </div>
-                        </div>
-                        <div className='review_change'>
-                            <span><a>1</a></span>
-                            <span><a>2</a></span>
-                            <span><a>3</a></span>
-                            <span><a>4</a></span>
-                            <span><a>5</a></span>
-                            <span><a>6</a></span>
-                            <span><a>7</a></span>
-                            <span><a>8</a></span>
-                            <span><a>9</a></span>
-                            <span><a>10</a></span>
+                            <div className='review_body'>
+                                <h3>리뷰제목</h3>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                            </div>
+                            <div className='review_body'>
+                                <h3>리뷰제목</h3>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                            </div>
+                            <div className='review_body'>
+                                <h3>리뷰제목</h3>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                                <p>리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란리뷰적는란</p>
+                            </div>
+                            <div className='review_change'>
+                                <span><a>1</a></span>
+                            </div>
                         </div>
                     </div>
                 </div>
